1ee079fc50b47748a9e1d375d5f7926b8b427a52
farzad-sedaghatbin <farzad.sedahatbin@gmail.com>
2019-06-16 22:18:01 +0430

    Revert "first commit"
    This reverts commit 11ab7aab

--- src/main/java/ir/redmind/paasho/config/CacheConfiguration.java
+++ src/main/java/ir/redmind/paasho/config/CacheConfiguration.java
@@ -1,19 +1,17 @@
 package ir.redmind.paasho.config;
 
-import java.time.Duration;
-
-import org.ehcache.config.builders.*;
-import org.ehcache.jsr107.Eh107Configuration;
-
-import io.github.jhipster.config.jcache.BeanClassLoaderAwareJCacheRegionFactory;
 import io.github.jhipster.config.JHipsterProperties;
-
+import io.github.jhipster.config.jcache.BeanClassLoaderAwareJCacheRegionFactory;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.jsr107.Eh107Configuration;
 import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.serviceregistry.Registration;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
