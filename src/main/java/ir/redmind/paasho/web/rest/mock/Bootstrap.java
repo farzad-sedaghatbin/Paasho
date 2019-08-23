a28d1ae1a897c3ce6b095515a6495774fb90ba42
farzad-sedaghatbin <farzad.sedahatbin@gmail.com>
2019-08-23 13:46:29 +0430

    dsfgdfgdf

--- src/main/java/ir/redmind/paasho/web/rest/mock/Bootstrap.java
+++ src/main/java/ir/redmind/paasho/web/rest/mock/Bootstrap.java
@@ -71,9 +71,9 @@ public class Bootstrap {
         List<User> users = userRepository.findAll();
         users.parallelStream().forEach(u -> {
             List<Contact> c = contactRepository.findByUser(u);
-            if (u.getTelegram() == null || u.getTelegram().length() == 0)
+            if ((u.getTelegram() == null || u.getTelegram().length() == 0 )&&(c!=null && c.size()>0))
                 u.setTelegram(c.stream().filter(c1 -> c1.getType().equals(ContactType.TELEGRAM)).findFirst().get().getValue());
-            if (u.getInstagram() == null || u.getInstagram().length() == 0)
+            if ((u.getInstagram() == null || u.getInstagram().length() == 0)&&(c!=null && c.size()>0))
                 u.setInstagram(c.stream().filter(c1 -> c1.getType().equals(ContactType.INSTAGRAM)).findFirst().get().getValue());
 
             userRepository.save(u);
